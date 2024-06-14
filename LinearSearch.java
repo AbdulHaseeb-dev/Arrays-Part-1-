c49474b491c33d3fa19a89c7279e54222dcc03e2
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-06-14 21:24:35 +0500

    Added another Function of linear search

--- LinearSearch.java
+++ LinearSearch.java
@@ -7,10 +7,21 @@ public class LinearSearch {
             } 
         }
         return -1;
+        }
 
-    }
+    public static int linearsearchfood (String food [], String lock) {
+        for (int i=0; i<food.length; i++) {
+            if (food[i].equals(lock)) {
+                return i;
+            }
+        }
+        return -1;
+        }
+
     public static void main(String[] args) {
         int numbers [] = {2,4,6,8,10,12,14,16};
+        String food [] = {"mango","orange","apple","banana","strawberry"};
+        String lock = "apple"; 
         int key = 10;
 
         int index = linearsearch(numbers,key);
@@ -18,6 +29,13 @@ public class LinearSearch {
             System.out.println("Not Found");
         } else {
             System.out.println("key is at index : " + index);
+        }
+
+        int foodIndex = linearsearchfood(food, lock);
+        if (foodIndex == -1) {
+            System.out.println("Not found");
+        } else {
+            System.out.println("lock is at index : " + foodIndex);
         }
     }
 }

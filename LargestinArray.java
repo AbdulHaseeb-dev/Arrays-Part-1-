fd69a4e7b80e5eea4b3f1e6a6bebf14f326730e1
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-06-15 23:07:13 +0500

    Found the smallest value in the Array

--- LargestinArray.java
+++ LargestinArray.java
@@ -1,12 +1,17 @@
 public class LargestinArray {
 
     public static int largestnumber (int numbers []) {
+        int smallest = Integer.MAX_VALUE;
         int largest = Integer.MIN_VALUE;
         for (int i=0; i<numbers.length; i++) {
             if (largest < numbers[i]) {
                 largest = numbers[i];
             }
+            if (smallest > numbers[i]) {
+                smallest = numbers[i];
+            }
         }
+        System.out.println("The smallest number is : " + smallest);
         return largest;
     }
     public static void main(String[] args) {
